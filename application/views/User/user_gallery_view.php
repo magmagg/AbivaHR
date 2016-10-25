a849d0529bf9beff9b44c191183ee16164d84703
magmagg <lorenzomagno2004@gmail.com>
2016-10-25 17:36:20 +0800

    Gallery

--- application/views/User/user_gallery_view.php
+++ application/views/User/user_gallery_view.php
@@ -79,30 +79,7 @@
 				</div>
 			</div>
 		</div>
-    <!-- Hidden video div -->
-    <div style="display:none;" id="video1">
-        <video class="lg-video-object lg-html5 video-js vjs-default-skin" controls preload="none">
-            <source src="videos/video1.mp4" type="video/mp4">
-             Your browser does not support HTML5 video.
-        </video>
-    </div>
-    <div style="display:none;" id="video2">
-        <video class="lg-video-object lg-html5 video-js vjs-default-skin" controls preload="none">
-            <source src="videos/video2.mp4" type="video/mp4">
-             Your browser does not support HTML5 video.
-        </video>
-    </div>
 
-    <!-- data-src should not be provided when you use html5 videos -->
-    <ul id="video-gallery">
-      <li data-poster="video-poster1.jpg" data-sub-html="video caption1" data-html="#video1" >
-          <img src="img/thumb1.jpg" />
-      </li>
-      <li data-poster="video-poster2.jpg" data-sub-html="video caption2" data-html="#video2" >
-          <img src="img/thumb2.jpg" />
-      </li>
-      ...
-    </ul>
 		<!-- PAGE CONTENT ENDS -->
 	</div>
 	<!-- /.col -->
