7847502b852a201dd67368969fa3537cfb69178a
alymimay <alygarciamimay@gmail.com>
2016-10-27 10:24:44 +0800

    ew

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -253,6 +253,19 @@ class Admin extends CI_Controller
     	}
 
   }
+
+  function view_other_profile()
+  {
+    $header['active_head'] = '';
+    $header['active_page'] = basename($_SERVER['PHP_SELF'], ".php");
+
+
+    $ID = $this->uri->segment(3);
+		$data['userDetails'] = $this->Admin_model->get_one_user($ID);
+      //var_dump($this->session->all_userdata());
+    $this->load->view('Admin/admin_header',$header);
+    $this->load->view('Admin/admin_view_other_profile',$data);
+  }
   //============================MESSAGES===================================//
   function messages()
   {
